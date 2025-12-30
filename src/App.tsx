94933754b1dc1c2cdbdf296d90befe2fc8687905
koko1056 <wmwnp32489@yahoo.co.jp>
2025-12-30 15:45:26 +0900

    Fix: Switch to HashRouter for iOS compatibility and configure deep link scheme in Info.plist

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { useDeepLinks } from "@/hooks/useDeepLinks";
 import Index from "./pages/Index";
 import Settings from "./pages/Settings";
@@ -40,9 +40,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <AppContent />
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
